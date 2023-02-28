5cf5e33f6afe5529bfebf3067bd9a719a178b0a1
Lauryn Gore <lauryngore@gmail.com>
2023-02-28 10:00:54 +0000

    Home page components.

---
+++ movies-start/movies/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const rootElement = createRoot(document.getElementById("root"));
+rootElement.render(<App />);
